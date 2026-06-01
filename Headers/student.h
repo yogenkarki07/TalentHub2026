627ebbea133d05cea8bddd4cfe92a4401bf26d3e
yogenkarki07 <yogeen399@gmail.com>
2026-06-02 08:52:56 +1200

    created student cpp and header file

---
+++ Headers/student.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+struct student{
+    string firstname;
+    string lastname;
+    int age;
+    string email;
+    string password;
+    string phone;
+    string address;
+};
+
+void StudentRegistration();
---
+++ student.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Headers/student.h"
+
+void StudentRegistration(){
+
+}
