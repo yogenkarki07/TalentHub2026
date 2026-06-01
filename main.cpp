74b731491d276fba31b9df02345cc15861705ea2
yogenkarki07 <yogeen399@gmail.com>
2026-06-01 13:09:53 +1200

    created seperated menu.cpp file and menu.h file

--- main.cpp
+++ main.cpp
@@ -1,54 +1,29 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
-#include <numeric>
+#include "Headers/menu.h"
 
-int main() {
-    // Total internal width required to frame the "WELCOME TO" block cleanly
-    const int INNER_WIDTH = 105;
-    std::string horizontalBorder = "+" + std::string(INNER_WIDTH + 2, '-') + "+";
+ int main() {
+     int choice;
+     do {
+         DisplayMenu();
+         std::cin >> choice;
 
-    // Top border
-    std::cout << horizontalBorder << "\n";
-    std::cout << "| " << std::string(INNER_WIDTH, ' ') << " |\n"; // Top spacer
-
-    std:: cout << R"(
-     |          _ _ _  ______   __      _____   ___     ___  ___  ______       _____    ___            |
-     |         | | | | | ____  |  |    / /    // _ \\  |  \\//  | | ____      | --- | // _ \\          |
-     |         | | | | | |___  |  |___ | |   || |-| || | | || | | | |___        | |  || |-| ||         |
-     |         |_____| |______ |__|___ \_\___ \\___//  |_|\\//|_| |______       |_|   \\___//          |
-     |           _____     _       _       ______  _   _  _______      _    _ _    _ ____              |
-     |          |_   _|   / \     | |     |  ____ | \ | ||__   __|    | |  | | |  | |  _ \             |
-     |            | |    / _ \    | |     | |__   |  \| |   | |       | |__| | |  | | |_) |            |
-     |            | |   / ___ \   | |     |  __|  | . ` |   | |       |  __  | |  | |  _ <             |
-     |            | |  /_/   \_\  | |____ | |____ | |\  |   | |       | |  | | |__| | |_) |            |
-     |            |_| / /     \ \ |______ |______ |_| \_|   |_|       |_|  |_|\____/|____/             |
-     |                                                                                                 |
-     |                                                                                                 |
-     |                               Connecting skills with opportunity                                |
-     |                                                                                                 |
-)";
-
-    std::cout << "| " << std::string(INNER_WIDTH, ' ') << " |\n";
-    std::cout << horizontalBorder << "\n";
-
-    std::cout << "| " << std::string(INNER_WIDTH, ' ') << " |\n";
-    std::cout << std::setw(60) << " Why are you here ?" << "\n";
-    std::cout << std::setw(80) << "-----------------------------------------------------------------" << "\n" << "\n";
-    std::cout << std::setw(20) << "|     " << "1. Student Registration " << std::setw(17) << "|       " << "3. Student Login " << "  |" << "\n" "\n";
-    std::cout << std::setw(80) << "-----------------------------------------------------------------" << "\n" << "\n" ;
-    std::cout << std::setw(20) << "|     " << "2. Admin Login " << std::setw(27) << "|        " << "4. Exit" << "           |" << "\n" "\n";
-    std::cout << std::setw(80) << "-----------------------------------------------------------------" << "\n";
-
-    std::cout << "| " << std::string(INNER_WIDTH, ' ') << " |\n";
-
-
-
-    std::cout << horizontalBorder << "\n";
-
-
-
-
-
-    return 0;
-}
+         switch (choice){
+         case 1:
+          //   StudentRegistration();
+             break;
+         case 2:
+         //    AdminLogin();
+             break;
+         case 3:
+          //   StudentLogin();
+             break;
+         case 4:
+             std::cout << "Thank you for visiting Talent Hub !" << "\n";
+             break;
+         default:
+             std::cout << "Invalid Choice. Please, choose Between 1 to 4." << "\n";
+             break;
+         }
+     } while (choice != 4);
+     return 0;
+ }
