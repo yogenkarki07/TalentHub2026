5fe0df892edc3b3ee4c7afe597cffbf718b41ab0
Yogen Karki <yogeen399@gmail.com>
2026-06-05 11:55:00 +1200

    Loading data WIP

--- filehandler.cpp
+++ filehandler.cpp
@@ -6,11 +6,12 @@
 #include "Headers/filehandler.h"
 using namespace std;
 
+// Save and store data after registration
 void SaveStudentInfo(vector<Student>& students) {
     Student s;
     ofstream file ("../File/students.csv", ios ::app);
     if (!file.is_open()){
-        cout << "\n[ERROR] Could not save data! 'students.csv' could not be opened.\n";
+        cout << "\nError! Could not save data! 'students.csv' could not be opened.\n";
         return;
     }
 
@@ -20,4 +21,17 @@ void SaveStudentInfo(vector<Student>& students) {
 
     }
     file.close();
+}
+
+// Load the stored data to Log-in
+void LoadStudentInfo(vector<Student>& students) {
+    Student s;
+    ifstream file ("../File/students.csv", ios ::in);
+    if (!file.is_open()) {
+        cout << "File does not exist." << endl;
+    }
+
+    for (Student s : students) {
+
+    }
 }
